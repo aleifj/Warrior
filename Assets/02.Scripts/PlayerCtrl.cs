47040b7de5bd419f036e1dae90d79f31597f9eaa
aleifj <sonmja96@naver.com>
2024-10-11 15:44:07 +0900

    input system과 일반 코딩의 비교
    1011

--- Assets/02.Scripts/PlayerCtrl.cs
+++ Assets/02.Scripts/PlayerCtrl.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using Unity.VisualScripting.Dependencies.NCalc;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -10,14 +11,43 @@ public class PlayerCtrl : MonoBehaviour
     private Vector3 moveDir;
     private float moveSpeed = 4.0f;
 
+    private PlayerInput playerInput;
+    private InputActionMap mainActionMap;
+    private InputAction moveAction;
+    private InputAction attackAction;
+
     private void Start()
     {
         anim = GetComponent<Animator>();
         transform = GetComponent<Transform>();
+        playerInput = GetComponent<PlayerInput>();
+
+        mainActionMap = playerInput.actions.FindActionMap("PlayerActions");
+
+        moveAction = mainActionMap.FindAction("Move");
+        attackAction = mainActionMap.FindAction("Attack");
+
+        moveAction.performed += ctx =>
+        {
+            Vector2 dir = ctx.ReadValue<Vector2>();
+            moveDir = new Vector3(dir.x, 0, dir.y);
+            anim.SetFloat("Movement", dir.magnitude);
+        };
+        moveAction.canceled += ctx =>
+        {
+            moveDir = Vector3.zero;
+            anim.SetFloat("Movement", 0.0f);
+        };
+
+        attackAction.performed += ctx =>
+        {
+            Debug.Log("Attack by c# event");
+            anim.SetTrigger("Attack");
+        };
     }
     private void Update()
     {
-        if(moveDir != Vector3.zero)
+        if (moveDir != Vector3.zero)
         {
             //진행 방향으로 회전
             transform.rotation = Quaternion.LookRotation(moveDir);
@@ -25,6 +55,7 @@ public class PlayerCtrl : MonoBehaviour
             transform.Translate(Vector3.forward * Time.deltaTime * moveSpeed);
         }
     }
+#region SEND_MESSEGE
     private void OnMove(InputValue value)
     {
         Vector2 dir = value.Get<Vector2>();
@@ -39,4 +70,28 @@ public class PlayerCtrl : MonoBehaviour
         Debug.Log("Attack");
         anim.SetTrigger("Attack");
     }
+#endregion SEND_MESSEGE
+
+
+#region UNITY_EVENTS
+    public void OnMove(InputAction.CallbackContext ctx)
+    {
+        Vector2 dir = ctx.ReadValue<Vector2>();
+        //2차원 좌표를 3차원 좌표로 변환.
+        moveDir = new Vector3(dir.x, 0, dir.y);
+        //Warrior_Run에니매이션 실행.
+        anim.SetFloat("Movement", dir.magnitude);
+    }
+
+    public void OnAttack(InputAction.CallbackContext ctx)
+    {
+        Debug.Log($"ctx.phase={ctx.phase}");
+
+        if(ctx.performed)//키 입력받고 실행이 완료되었을 때 performed
+        {
+            Debug.Log("Attack");
+            anim.SetTrigger("Attack");
+        }
+    }
+#endregion UNITY_EVENTS
 }
